5efa23dc6b33a2f36501d5b8752a80e98b6c368c
Emma Maikuri <maikuriemma@gmail.com>
2022-10-10 13:57:11 +0300

    ejs, Lecturer Moudule

--- index.js
+++ index.js
@@ -1,18 +1,52 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const fs = require('fs');
-const app = express();
-const port = 3000;
-const jsonParser = bodyParser.json();
-//const fileName = 'coursess.json';
+const express = require("express"),
+    app = express();
 
-app.set('views', 'views');
-app.set('view engine', 'hbs');
-app.use(express.static('public'));
+//setting view engine to ejs
+app.set("view engine", "ejs");
 
-app.get('/', (request, response) => {
-    response.render('index');
+//route for index page
+app.get("/", function (req, res) {
+
+   res.render("index");
 });
 
-app.listen(port);
-console.log('server listening on port 3000');
+app.get("/home", function (req, res) {
+
+   res.render("home");
+});
+
+app.get("/lecturer", function (req, res) {
+   var characters = [
+      {
+         course: 'APT3040',
+         title: "Java Programming"
+      },
+      {
+         course: 'APP4035',
+         title: "Framweorks"
+      },
+      {
+         course: 'APT3060',
+         title: "Mobile Programming"
+      },
+      {
+         course: 'APT3020',
+         title: "Artificial Intelligence"
+      }
+   ];
+ const subheading = "The Courses you Teach";
+
+   res.render("lecturer", {
+      characters: characters,
+      subheading: subheading
+   });
+});
+
+//route for student page
+app.get("/student", function (req, res) {
+   res.render("student");
+});
+
+app.listen(3000, function () {
+   console.log("Server is running on port 3000 ");
+});
